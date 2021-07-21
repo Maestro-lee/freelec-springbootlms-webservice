3651aea73c0f028c4f93dc7a53d01cc720233e61
DESKTOP-368100P\82107 <alstlr815@naver.com>
2021-07-21 22:15:20 +0900

    api생성 및 글 등록화면 생성

--- src/main/java/org/example/springboot/domain/posts/Posts.java
+++ src/main/java/org/example/springboot/domain/posts/Posts.java
@@ -3,6 +3,7 @@ package org.example.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.springboot.domain.BaseTimeEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,6 +33,11 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
     }
 
 }
